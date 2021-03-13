52b3fba92be9514b88be58835325e9c374c2d635
Oliver Altergott <altergott.oliver@gmail.com>
2021-03-13 16:33:02 +0100

    redirect https

--- server/index.js
+++ server/index.js
@@ -23,8 +23,18 @@ if (!isDev && cluster.isMaster) {
 } else {
     const app = express();
 
+    if (process.env.NODE_ENV === 'production') {
+        app.use((req, res, next) => {
+            if (req.header('x-forwarded-proto') !== 'https')
+                res.redirect(`https://${req.header('host')}${req.url}`)
+            else
+                next()
+        })
+    }
+
     // Priority serve any static files.
     app.use(express.static(path.resolve(__dirname, '../frontend-react/build')));
+
 
     // Answer API requests.
     app.get('/api', function (req, res) {
